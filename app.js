bf89337b33a5e0e03f7542e4dd75153385995439
Naveed786Ahmed <138322439+Naveed786Ahmed@users.noreply.github.com>
2023-12-02 20:58:54 +0500

    Palindrome String in Javascript

---
+++ app.js
@@ -0,0 +1,10 @@
+var str = prompt("Please enter a String: ")
+var len = str.length
+var msg = "It's a palindrome"
+
+for(var i = 0; i < len/2; i++){
+    if(str[i] != str[len -1 -i]){
+        console.log(str + " It's not a palindrome")
+    }
+}
+console.log(str + " " + msg)
